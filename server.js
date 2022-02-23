19c3903bacc8e47e96fc2e4ef22b192eb3fff2de
Anwar Hossain <mdanwarhossain1996bd@gmail.com>
2022-02-22 23:54:39 -0600

    prepared to deploy in Heroku

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const dotenv=require('dotenv');
-dotenv.config();
+dotenv.config(); 
 const app=require('./app')
 const mongoose =require('mongoose');
 
-mongoose.connect(process.env.MONGODB_SERVER,{
+const DB=process.env.MONGODB_SERVER.replace('<PASSWORD>',process.env.DB_PASSWORD)
+
+mongoose.connect(DB,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    
 })
 .then(()=>console.log("Connected to MongoDB"))
 .catch(err=>console.log(err))
